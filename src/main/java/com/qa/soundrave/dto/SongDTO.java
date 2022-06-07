a441f8e0b88fc6c9a85a2a8b797867d23f78de1f
Peter-Dagley <peter_dagley@outlook.com>
2022-06-07 11:44:05 +0100

    fixing error in SongDTO

--- src/main/java/com/qa/soundrave/dto/SongDTO.java
+++ src/main/java/com/qa/soundrave/dto/SongDTO.java
@@ -6,12 +6,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class SongDTO {
-
+	
 	private Long id;
 	private String title;
-//	private String artist;
 	private String genre;
 	private double length;
-
-//	private Artist artist;
+	
 }

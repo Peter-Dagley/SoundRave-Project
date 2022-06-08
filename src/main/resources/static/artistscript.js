83056c87c5f03737eb9702015d5ccf1357b22ec6
Peter-Dagley <peter_dagley@outlook.com>
2022-06-08 13:11:53 +0100

    finished adding functionality to webpage

--- src/main/resources/static/artistscript.js
+++ src/main/resources/static/artistscript.js
@@ -49,6 +49,31 @@ let deleteArtist = (id) => {
 
 deleteArtistButton.onclick = () => deleteArtist(deleteArtistInput.value);
 
+// Update Artist
+function updateArtist(id, name) {
+    if (id == 0 || name == "") {
+        console.log("Empty fields.")
+        alert("Empty fields.")
+    } else {
+        fetch(`http://localhost:8080/artist/update/${id}`, {
+            method: 'put',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                "name": name,
+            }),
+        })
+            .then((response) => { if (response.status == 201) { location.reload(); } console.log(response) })
+            .catch((error) => console.log(error));
+    }
+}
+
+updateArtistButton.onclick = () => updateArtist(
+    updateIDArtistInput.value,
+    updateNameArtistInput.value,
+);
+
 // Create Song
 function createSong(id, title, artist, genre, length) {
     if (id == 0 || title == "" || artist == "" || genre == "" || length == 0) {
@@ -80,23 +105,24 @@ addSongButton.onclick = () => createSong(
     lengthInput.value
 );
 
-
-// Show All Artists
-let searchAll = () => {
-    fetch(`http://localhost:8080/artist/read`)
-
-        .then((response) => {
-            if (response.status !== 200) {
-                console.error(`status: ${response.status}`);
-                return;
+// Delete Song
+let deleteSong = (id) => {
+    if (id == 0) {
+        console.log("Empty fields.")
+        alert("Empty fields.")
+    } else {
+        fetch(`http://localhost:8080/song/delete/${id}`, {
+            method: 'delete',
+            headers: {
+                'content-type': 'application/json',
             }
-            response.json().then((data) => {
-                console.log(response.status);
-                console.table(data);
-                addItem(data);
-            });
-        }).catch((error) => console.error(`Request failed: ${error}`));
+        })
+            .then((response) => console.log(response))
+            .catch((error) => console.log(error));
+    }
 }
+
+deleteSongButton.onclick = () => deleteSong(deleteSongInput.value);
 
 // Add item to list on Site
 function addItem(data) {
@@ -114,3 +140,55 @@ function addItem(data) {
 showArtistButton.addEventListener("click", () => {
     searchAll();
 });
+
+// Update Song
+function updateSong(id2, name, id, title, genre, length) {
+    if (id == 0 || title == "" || genre == "" || length == 0 || id2 == 0 || name == "") {
+        console.log("Empty fields.")
+        alert("Empty fields.")
+    } else {
+        fetch(`http://localhost:8080/song/update/${id}`, {
+            method: 'put',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                "title": title,
+                "artist": {
+                    "id": id2,
+                    "name": name
+                },
+                "genre": genre,
+                "length": length
+            }),
+        })
+            .then((response) => { if (response.status == 201) { location.reload(); } console.log(response) })
+            .catch((error) => console.log(error));
+    }
+}
+
+updateSongButton.onclick = () => updateSong(
+    updateSongArtistIDInput.value,
+    updateSongArtistNameInput.value,
+    updateIDSongInput.value,
+    updateTitleInput.value,
+    updateGenreInput.value,
+    updateLengthInput.value
+);
+
+// Show All Artists
+let searchAll = () => {
+    fetch(`http://localhost:8080/artist/read`)
+
+        .then((response) => {
+            if (response.status !== 200) {
+                console.error(`status: ${response.status}`);
+                return;
+            }
+            response.json().then((data) => {
+                console.log(response.status);
+                console.table(data);
+                addItem(data);
+            });
+        }).catch((error) => console.error(`Request failed: ${error}`));
+}
